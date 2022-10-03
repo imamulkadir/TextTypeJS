1b0539ab9c3a0dd5d2414006d921badeac5df481
Imamul Kadir <imamulkadiroyan@gmail.com>
2022-10-03 20:20:01 +0600

    update text type

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
-var title = document.querySelector('h1');
+var count = 0;
 
-var text = "Thank you for existing ❤️";
+var currentText = 0;
 
 var index = 0;
 
-setInterval(fun, 500);
+var letter = 0;
 
-function fun(){
-    title.innerHTML = text.slice(0, index++);
-    if(index>text.length){
+var text = ["Thank you for existing ❤️", "You are beautiful ❤️", "You can Do it ❤️"];
+
+
+
+setInterval(myType, 100);
+
+function myType(){
+    currentText = text[count];
+    letter = currentText.slice(0, index++);
+    var title = document.querySelector('h1').innerHTML = letter;
+
+    if(letter.length == currentText.length){
+        count++;
         index = 0;
+    }
+
+    if(count == text.length){
+        count = 0;    
     }
 }
